714ebce22b049b7163db6aad51583e4de2f5d7d4
Xavier Perez <duosrx@gmail.com>
2015-03-07 15:29:36 -0800

    Replace the program with a List

--- src/main/scala/Hi.scala
+++ src/main/scala/Hi.scala
@@ -1,67 +1,50 @@
-import scala.util.parsing.combinator._
+import scala.util.parsing.combinator.RegexParsers
 
 object BF extends App {
   val raw = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>."
-  val program = BFParser.parseAll(BFParser.program, raw).get.toArray
+  val program = BFParser.parseAll(BFParser.program, raw).get
+  // println(program)
   val vm = new VM(program)
   vm.run
 }
 
-sealed abstract class Command
+trait Command
 case class Move(n: Int) extends Command
 case class Add(n: Int) extends Command
-case object Read extends Command
-case object Write extends Command
-case object LoopIn extends Command
-case object LoopOut extends Command
+case class Read() extends Command
+case class Write() extends Command
+case class Loop(commands: List[Command]) extends Command
 
-object BFParser extends JavaTokenParsers {
+object BFParser extends RegexParsers {
+  def ignore = """[^<>+-\.,\[\]]""".r
   def add     = "+" ^^ { s => Add(1) }
   def dec     = "-" ^^ { s => Add(-1) }
   def left    = "<" ^^ { s => Move(-1) }
   def right   = ">" ^^ { s => Move(1) }
-  def read    = "," ^^ { s => Read }
-  def write   = "." ^^ { s => Write }
-  def loopin  = "[" ^^ { s => LoopIn }
-  def loopout = "]" ^^ { s => LoopOut }
+  def read    = "," ^^ { s => Read() }
+  def write   = "." ^^ { s => Write() }
 
-  def operator = (add | dec | left | right | read | write | loopin | loopout)
-  def program = operator *
+  def operator = (add | dec | left | right | read | write)
+
+  def loop    = "[" ~> (operator *) <~ "]" ^^ { l => Loop(l) }
+  def program = (operator | loop) *
 }
 
-class VM(program: Array[_ <: Command]) {
+class VM(program: List[Command]) {
   var mem: Array[Integer] = Array.fill[Integer](100)(0)
   var mp = 0
-  var pc = 0
 
-  def run = while (pc < program.size) step(program(pc))
+  def run: Unit = run(program)
 
-  def findLoop(direction: Int): Int = {
-    def go(current: Int, loop: Int): Int = {
-      if (loop <= 0) current
-      else {
-        val next = current + direction
-        program(current + direction) match {
-          case LoopIn  => go(next, loop + direction)
-          case LoopOut => go(next, loop - direction)
-          case _         => go(next, loop)
-        }
-      }
-    }
-
-    go(pc + direction, 1)
+  private def run(commands: List[Command]): Unit = {
+    commands.map(step)
   }
 
-  def step(command: Command) = {
-    command match {
-      case Add(n)  => mem(mp) += n
-      case Move(n) => mp += n
-      case Write   => print(mem(mp).toChar)
-      case Read    => mem(mp) = readInt()
-      case LoopIn  => if (mem(mp) == 0) pc = findLoop(1)
-      case LoopOut => if (mem(mp) != 0) pc = findLoop(-1)
-    }
-
-    pc += 1
+  private def step(command: Command) = command match {
+    case Add(n)  => mem(mp) += n
+    case Move(n) => mp += n
+    case Write() => print(mem(mp).toChar)
+    case Read()  => mem(mp) = readInt()
+    case Loop(l) => do { run(l) } while (mem(mp) != 0)
   }
 }
